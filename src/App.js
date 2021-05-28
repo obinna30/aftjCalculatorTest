06ce5ad067e284744b68999cb459c9822c506d45
pharmcoder <ekwealorobinna112@gmail.com>
2021-05-28 02:19:14 +0100

    test done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+import VanillaTilt from "vanilla-tilt";
 
 function App() {
+  const numbers = ["9", "8", "7", "6", "5", "4", "3", "2", "1"];
+  const operations = ["/", "-", "+", "="];
+
+  const [perform, setPerform] = useState("");
+  const [getResult, setGetResult] = useState("");
+
+  const tilt = useRef(null);
+
+  const options = {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.5,
+  };
+
+  useEffect(() => {
+    VanillaTilt.init(tilt.current, options);
+  }, []);
+
+  const AnyClick = (val) => {
+    if (val.val !== "=") {
+      const result = perform.concat(val.val);
+      setPerform(result);
+    } else if (val.val === "=") {
+      if (
+        perform.endsWith("/") ||
+        perform.endsWith("-") ||
+        perform.endsWith("+")
+      ) {
+        console.log(perform);
+        const poped = perform.slice(0, -1);
+        console.log(poped);
+        const solved = eval(poped);
+        setGetResult(solved);
+        setPerform("");
+      } else {
+        const solved = eval(perform);
+        setGetResult(solved);
+        setPerform("");
+      }
+    }
+  };
+
+  function clearScreen(e) {
+    setPerform("");
+    setGetResult(0);
+    // e.preventDefault(window.location.reload());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator" ref={tilt}>
+        <div className="display">{getResult ? getResult : perform}</div>
+        <div className="buttons">
+          <div className="leftSide">
+            <div id="seeResult" onClick={clearScreen}>
+              Clear
+            </div>
+            <div className="numbers">
+              {numbers.map((val, key) => {
+                return (
+                  <div
+                    id="individualNumber"
+                    value={val}
+                    onClick={() => AnyClick({ val })}
+                  >
+                    {" "}
+                    {val}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="leftSide">
+            {operations.map((val, key) => {
+              return (
+                <div id="operations" onClick={() => AnyClick({ val })}>
+                  {" "}
+                  {val}{" "}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
